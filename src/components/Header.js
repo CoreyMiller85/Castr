39b3c9f99364be66b56d6710b64f266ca654e926
Corey Miller <coreymiller454@gmail.com>
2021-08-01 21:22:10 -0400

    skeleton init

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import SearchBar from "./SearchBar";
+
+const Header = (props) => {
+	return (
+		<div>
+			Header Component
+			<SearchBar />
+		</div>
+	);
+};
+
+export default Header;
---
+++ src/components/Main.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Main = () => {
+	return <div>Main Container</div>;
+};
+
+export default Main;
